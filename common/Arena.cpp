33379c33ad7f799a3f39d94b59f84a5c9c4f7d0d
Liraal2 <noywadr@gmail.com>
2016-04-12 22:18:57 +0200

    Added 2 new fruit type options.

--- common/Arena.cpp
+++ common/Arena.cpp
@@ -25,10 +25,11 @@ void Arena::spawnNewFruit() {
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> posX(0, width-1);
     std::uniform_int_distribution<> posY(0, height-1);
+    std::uniform_int_distribution<> type(1, 3);
     vec2 pos;
     do {
         pos = vec2(posX(gen), posY(gen));
     } while (!canPlaceFruit(pos));
     std::cerr << "Spawned fruit " << pos.x << ":" << pos.y << std::endl;
-    fruits.push_back(Fruit(1, pos));
+    fruits.push_back(Fruit(type(gen), pos));
 }
